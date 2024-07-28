2b00f032fc3fdb0b2ebdcace7cc903963e4712bd
saurabhprasadsah <kumarsaurabh2910@gmail.com>
2024-07-28 15:59:47 +0530

    interpolation

---
+++ src/components/Home.vue
@@ -0,0 +1,37 @@
+<template>
+<h1> Email : {{ email }}</h1>
+
+<h1> Name : {{ getName("SAURABH KUMAR") }}</h1>
+<h1> Name2 : {{ getData().email }}</h1>
+</template>
+
+<script>
+export default {
+    name: 'HomePage',
+    data() {
+        return {
+            email: 'kumarsaurabh2910@gmail.com',
+        }
+    },
+
+    methods: {
+        getName(name) {
+            return name
+        },
+        getData() {
+            return {
+                name: "saurabh",
+                email: this.email
+            }
+        }
+
+    }
+
+}
+</script>
+
+<style scoped>
+h1 {
+    color: blue
+}
+</style>
