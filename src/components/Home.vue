c832b1a487f3be92d3df9f729371a2f9d7438a17
saurabhprasadsah <kumarsaurabh2910@gmail.com>
2024-07-29 13:10:54 +0530

    home vue

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,37 +1,52 @@
 <template>
-<h1> Email : {{ email }}</h1>
 
-<h1> Name : {{ getName("SAURABH KUMAR") }}</h1>
+<!-- <h1> Name : {{ getName("SAURABH KUMAR") }}</h1>
 
 <h2>Name : {{ getName ("Raju kumar ")}}</h2>
 
 <h1> Name2 : {{ getData().email }}</h1>
 
-<h1>Name3 : {{getData().name }}</h1>
+<h1>Name3 : {{getData().name }}</h1> -->
+
+<button  v-on:click="getData()"> Click Me</button> 
+<br>
+<br>
+<button  v-on:dblclick="getData('Button double clicked')"> Click Me</button>
+
+<h2>{{ count }}</h2>
 
 </template>
 
 <script>
 export default {
     name: 'HomePage',
-    data() {
+   
+    data(){
         return {
-            email: 'kumarsaurabh2910@gmail.com',
+            count:0
         }
     },
 
-    methods: {
-        getName(name) {
-            return name
-        },
-        getData() {
-            return {
-                name: "saurabh",
-                email: this.email
-            }
-        }
 
-    }
+    methods:{
+        getData(data)
+        {
+            this.count=10
+        }
+    },
+
+    // methods: {
+    //     getName(name) {
+    //         return name
+    //     },
+    //     getData() {
+    //         return {
+    //             name: "saurabh",
+    //             email: this.email
+    //         }
+    //     }
+
+    // }
 
 }
 </script>
